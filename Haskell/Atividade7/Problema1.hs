8ed7b2a04dc86d25b9d5e77aba8fb99f6430d8b5
maykonmarcosjunior <maykon.marcos.junior.@grad.ufsc.br>
2023-09-28 08:28:23 -0300

    Incluindo arquivos para atividade XI

--- Haskell/Atividade7/Problema1.hs
+++ Haskell/Atividade7/Problema1.hs
@@ -3,8 +3,8 @@ Leia o cap ́ıtulo 5 da apostila do professor Dovicchi, dispon ́ıvel no Moodl
 A: O que s ̃ao classes primitivas e secund ́arias no Haskell? Cite 3 exemplos de classes primitivas e 3
 exemplos de classes secund ́arias. Cite o papel de cada uma das classes citadas e alguns m ́etodos
 existentes nelas, bem como o que esses m ́etodos fazem.
-B: Fa ̧ca um breve resumo sobre as classes num ́ericas do Haskell. Como elas est ̃ao relacionadas entre si?
-Tente entender os m ́etodos dispon ́ıvel em cada classe.
+B: Fa ̧ca um breve resumo sobre as classes numericas do Haskell. Como elas est ̃ao relacionadas entre si?
+Tente entender os metodos disponıvel em cada classe.
 --}
 
 main :: IO ()
